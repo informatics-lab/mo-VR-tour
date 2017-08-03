a90ff888bacb40f08c6271400e7060f648cd329f
Ruth Price <ruth.price@informaticslab.co.uk>
2017-08-03 15:04:28 +0100

    Add mouse-hover animation to all menu buttons

--- HpcTour/components/scenes/layouts/elements/DataStoreMenuButton.js
+++ HpcTour/components/scenes/layouts/elements/DataStoreMenuButton.js
@@ -1,22 +1,71 @@
 import React from 'react';
 import {
+  Animated,
   asset,
   Image,
   VrButton
 } from 'react-vr';
+import { Easing } from 'react-native';
 
 class DataStoreMenuButton extends React.Component {
+  constructor() {
+    super();
+    this.state={
+      borderColorAnim: new Animated.Value(0),
+      borderWidthAnim: new Animated.Value(0.025),
+    }
+  }
+
+  handleEnterBorderAnim() {
+    Animated.parallel([
+      Animated.timing(
+        this.state.borderColorAnim, {
+          toValue: 1,
+          easing: Easing.out(Easing.exp)
+        }
+      ),
+      Animated.timing(
+        this.state.borderWidthAnim, {
+          toValue: 0.03,
+          easing: Easing.out(Easing.exp)
+        }
+      )]
+    ).start();
+  }
+
+  handleExitBorderAnim() {
+    Animated.parallel([
+      Animated.timing(
+        this.state.borderColorAnim, {
+          toValue: 2,
+          easing: Easing.out(Easing.exp)
+        }
+      ),
+      Animated.timing(
+        this.state.borderWidthAnim, {
+          toValue: 0.025,
+          easing: Easing.out(Easing.exp)
+        }
+      )]
+    ).start();
+  }
+
   render() {
     return (
       <VrButton
-        onClick={() => this.props.handleMenuSelect(2)}>
-        <Image
+        onClick={() => this.props.handleMenuSelect(2)}
+        onEnter={() => this.handleEnterBorderAnim()}
+        onExit={() => this.handleExitBorderAnim()}>
+        <Animated.Image
           source={asset('DataStoreMenuScreenshot.png')}
           style={{
             width: 1.096,
             height: 0.69,
-            borderColor: 'greenyellow',
-            borderWidth: 0.025
+            borderColor: this.state.borderColorAnim.interpolate({
+              inputRange: [0, 1, 2],
+              outputRange: ['greenyellow', 'white', 'greenyellow']
+            }),
+            borderWidth: this.state.borderWidthAnim
           }}
         />
       </VrButton>
